a608d8e7e3e34e8bbcfbb083fceaae5f29960f1b
victormanuel20 <victormanuel20034@gmail.com>
2025-08-25 19:53:29 -0500

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sumanumeros1" 
---
+++ src/main/java/org/example/Manager.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Manager {
+    private list<Integer> numeros;
+
+    public Manager() {
+        this.numeros = new ArrayList<>();
+    }
+    public void addNumero(Int numero) {
+        this.numeros.add(numero);
+    }
+}
